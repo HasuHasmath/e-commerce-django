d93eaf043fb53ac084fc7eec2e5790c47d81e939
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-13 23:55:14 +0530

    defining pages

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', HomePage)
+    path('home/', HomePage),
+    path('about/', AboutPage),
+    path('service/', ServicePage),
+    path('contact/', ContactPage),
+
 ]
--- Inventory/views.py
+++ Inventory/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 
+
 def HomePage(request):
 
     data = {
         "name": "Hasmath",
         "role": "manager",
-        "numbers" : [1,2,3,4,5,6],
-        "marks" : {
-            "tamil" : "100",
-            "maths" : "97",
-            "english" : "85"
+        "numbers": [1, 2, 3, 4, 5, 6],
+        "marks": {
+            "tamil": "100",
+            "maths": "97",
+            "english": "85"
         }
 
     }
 
-    return render(request,'index.html', data)
+    return render(request, 'home.html', data)
 
-  
+
+def AboutPage(request):
+
+    return render(request, 'about.html')
+
+
+def ServicePage(request):
+
+    return render(request, 'service.html')
+
+
+def ContactPage(request):
+
+    return render(request, 'contact.html')
