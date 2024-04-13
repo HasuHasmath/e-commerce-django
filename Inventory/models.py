100fecc97dc83abc1cd58f0141a3bafd7b44d841
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-14 01:57:41 +0530

    model creation

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    
+    product_name = models.CharField(max_length=200, null=True)
+    product_code = models.CharField(max_length=200, null=True)
+    price = models.FloatField( default=0)
+    vat = models.IntegerField(default=0)
+    food_product = models.BooleanField(default=False)
+
